65d47d540ebff551386557e2cc606a942f08d2a5
iolite-staff <123618102+iolite-staff@users.noreply.github.com>
2025-07-30 15:28:48 +0900

    Xアイコン 修正

--- cgi-bin/guitarroom501/catalog/index.php
+++ cgi-bin/guitarroom501/catalog/index.php
@@ -5,8 +5,8 @@ require_once('../inc/h_const.inc');
 require_once('../inc/h_file.inc');
 require_once('../inc/h_common.inc');
 
-$site_url ="http://demo.iolite.co.jp/guitarroom501/";
-$home_url ="http://demo.iolite.co.jp/guitarroom501/";
+// $site_url ="http://demo.iolite.co.jp/guitarroom501/";
+// $home_url ="http://demo.iolite.co.jp/guitarroom501/";
 //$site_url ="https://www.g-room501.com/";
 //$home_url ="https://www.g-room501.com/";
 
@@ -67,46 +67,53 @@ $home_url ="http://demo.iolite.co.jp/guitarroom501/";
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
 <meta name="format-detection" content="telephone=no, email=no, address=no">
 <title>Inventory | Guitar Room 501</title>
-<link rel="stylesheet" type="text/css" href="<?php echo $home_url ?>lib/bxslider.css">
-<link rel="stylesheet" type="text/css" media="all" href="<?php echo $home_url ?>css/common.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $home_url ?>css/screen.css" />
-<link rel="stylesheet" type="text/css" media="screen and (max-width:660px)" href="<?php echo $home_url ?>css/screen_sp.css" />
-<link rel="stylesheet" type="text/css" media="screen and (min-width:661px) and (max-width:1023px)" href="<?php echo $home_url ?>css/screen_tb.css" />
-<link rel="stylesheet" type="text/css" media="screen and (min-width:1024px)" href="<?php echo $home_url ?>css/screen_pc.css" />
-<link rel="stylesheet" href="<?php echo $home_url ?>css/fancybox.min.css" />
-<link rel="stylesheet" type="text/css" media="print" href="<?php echo $home_url ?>css/print.css" />
-<link rel="apple-touch-icon" href="<?php echo $home_url ?>apple-touch-icon.png" />
-<link rel="icon" type="image/png" href="<?php echo $home_url ?>favicon-.png" sizes="16x16">
-<link rel="icon" type="image/png" href="<?php echo $home_url ?>favicon.png" sizes="32x32">
-<link rel="stylesheet" type="text/css" href="<?php echo $home_url ?>lib/jquery/slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="<?php echo $home_url ?>lib/jquery/slick/slick-theme.css"/>
-<script type="text/javascript" src="<?php echo $home_url ?>lib/jquery/jquery-3.7.1.min.js"></script>
-<script type="text/javascript" src="<?php echo $home_url ?>lib/jquery/slick/slick.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://www.g-room501.com/lib/bxslider.css">
+<link rel="stylesheet" type="text/css" media="all" href="https://www.g-room501.com/css/common.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="https://www.g-room501.com/css/screen.css" />
+<link rel="stylesheet" type="text/css" media="screen and (max-width:660px)" href="https://www.g-room501.com/css/screen_sp.css" />
+<link rel="stylesheet" type="text/css" media="screen and (min-width:661px) and (max-width:1023px)" href="https://www.g-room501.com/css/screen_tb.css" />
+<link rel="stylesheet" type="text/css" media="screen and (min-width:1024px)" href="https://www.g-room501.com/css/screen_pc.css" />
+<link rel="stylesheet" href="https://www.g-room501.com/css/fancybox.min.css" />
+<link rel="stylesheet" type="text/css" media="print" href="https://www.g-room501.com/css/print.css" />
+<link rel="apple-touch-icon" href="https://www.g-room501.com/apple-touch-icon.png" />
+<link rel="icon" type="image/png" href="https://www.g-room501.com/favicon-.png" sizes="16x16">
+<link rel="icon" type="image/png" href="https://www.g-room501.com/favicon.png" sizes="32x32">
+<link rel="stylesheet" type="text/css" href="https://www.g-room501.com/lib/jquery/slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="https://www.g-room501.com/lib/jquery/slick/slick-theme.css"/>
+<script type="text/javascript" src="https://www.g-room501.com/lib/jquery/jquery-3.7.1.min.js"></script>
+<script type="text/javascript" src="https://www.g-room501.com/lib/jquery/slick/slick.min.js"></script>
 </head>
 <body>
 	<header>
-	<a href="<?php echo $home_url ?>index.html"><img src="<?php echo $home_url ?>img/interface/logo_header.png" alt="Guitar Room 501"></a>
+	<a href="https://www.g-room501.com/index.html"><img src="https://www.g-room501.com/img/interface/logo_header.png" alt="Guitar Room 501"></a>
 		<span class="pc"></span>
-		<a class="insta_link_sp" href="https://www.instagram.com/guitarroom501/?img_index=1" target="_blank"><img src="<?php echo $home_url ?>img/interface/insta_icon.png" alt="instagram"></a>
+		<div class="link_sp">
+			<a class="x_link_sp" href="https://x.com/guitarroom501" target="_blank"><img src="https://www.g-room501.com/img/interface/X-icon_b.png" alt="X"></a>
+			<a class="insta_link_sp" href="https://www.instagram.com/guitarroom501/?img_index=1" target="_blank"><img src="https://www.g-room501.com/img/interface/insta_icon.png" alt="instagram"></a>
+		</div>
 		<nav>
 			<ul>
-				<li class="menu-item"><a href="<?php echo $home_url ?>index.html">Home</a></li>
-				<li class="menu-item"><a href="<?php echo $home_url ?>inventory/index.html">Inventory</a>
+				<li class="menu-item"><a href="https://www.g-room501.com/index.html">Home</a></li>
+				<li class="menu-item"><a href="https://www.g-room501.com/inventory/index.html">Inventory</a>
 					<ul class="submenu">
-						<li><a href="<?php echo $home_url ?>inventory/category.html">All</a></li>
-						<li><a href="<?php echo $home_url ?>inventory/category1.html">Electric Guitar</a></li>
-						<li><a href="<?php echo $home_url ?>inventory/category2.html">Electric Bass</a></li>
-						<li><a href="<?php echo $home_url ?>inventory/category3.html">Acoustic Guitar</a></li>
-						<li><a href="<?php echo $home_url ?>inventory/category4.html">Others</a></li>
-						<li><a href="<?php echo $home_url ?>inventory/category5.html">Parts & Accessories</a></li>
+						<li><a href="https://www.g-room501.com/inventory/category.html">All</a></li>
+						<li><a href="https://www.g-room501.com/inventory/category1.html">Electric Guitar</a></li>
+						<li><a href="https://www.g-room501.com/inventory/category2.html">Electric Bass</a></li>
+						<li><a href="https://www.g-room501.com/inventory/category3.html">Acoustic Guitar</a></li>
+						<li><a href="https://www.g-room501.com/inventory/category4.html">Others</a></li>
+						<li><a href="https://www.g-room501.com/inventory/category5.html">Parts & Accessories</a></li>
 					</ul>
 				</li>
-				<li class="menu-item"><a href="<?php echo $home_url ?>shopguide/index.html">Shop Guide</a></li>
-				<li class="menu-item"><a href="<?php echo $home_url ?>buy-sell/index.html">Buy-Sell</a></li>
-				<li class="menu-item"><a href="<?php echo $home_url ?>makers/index.html">Makers</a></li>
-				<li class="menu-item"><a href="<?php echo $home_url ?>repairs/index.html">Repairs</a></li>
+				<li class="menu-item"><a href="https://www.g-room501.com/shopguide/index.html">Shop Guide</a></li>
+				<li class="menu-item"><a href="https://www.g-room501.com/buy-sell/index.html">Buy-Sell</a></li>
+				<li class="menu-item"><a href="https://www.g-room501.com/makers/index.html">Makers</a></li>
+				<li class="menu-item"><a href="https://www.g-room501.com/repairs/index.html">Repairs</a></li>
 			</ul>
-			<a class="insta_link_pc" href="https://www.instagram.com/guitarroom501/?img_index=1" target="_blank"><img src="<?php echo $home_url ?>img/interface/insta_icon.png" alt="instagram"></a>
+			<div class="link_pc">
+				<a class="x_link_pc" href="https://x.com/guitarroom501" target="_blank"><img src="https://www.g-room501.com/img/interface/X-icon_w.png" alt="X"></a>
+				<a class="insta_link_pc" href="https://www.instagram.com/guitarroom501/?img_index=1" target="_blank"><img src="https://www.g-room501.com/img/interface/insta_icon.png" alt="instagram"></a>
+			</div>
+
 		</nav>
 		<span id="nav_sp"><span></span><span></span></span>
 	</header>
@@ -327,7 +334,7 @@ for($type=1;$type<=5;$type++){
 		<div id="footer_content_area">
 			<div id="footer_info_area">
 				<div>
-					<img src="<?php echo $home_url ?>img/interface/logo_footer.png" alt="Guitar Room 501">
+					<img src="https://www.g-room501.com/img/interface/logo_footer.png" alt="Guitar Room 501">
 					<address>
 					<p>
 							〒101-0052<br>東京都千代田区神田小川町2-12 信愛ビル501
@@ -341,7 +348,7 @@ for($type=1;$type<=5;$type++){
 						</p>
 					</address>
 				</div>
-				<img src="<?php echo $home_url ?>img/interface/shop_pic.jpg" alt="店内画像">
+				<img src="https://www.g-room501.com/img/interface/shop_pic.jpg" alt="店内画像">
 			</div>
 			<div id="footer_schedule_area">
 <?php include_once("../top/calender2.php"); ?>
@@ -352,9 +359,9 @@ for($type=1;$type<=5;$type++){
 		</div>
 	</footer>
 <!-- js -->
-<script src="<?php echo $home_url ?>lib/jquery/jquery-3.3.1.min.js"></script>
+<script src="https://www.g-room501.com/lib/jquery/jquery-3.3.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
-<script type="text/javascript" src="<?php echo $home_url ?>js/global.js"></script>
+<script type="text/javascript" src="https://www.g-room501.com/js/global.js"></script>
 <script type="text/javascript">
 	$(document).ready(function(){
 		$('.slider').bxSlider({
